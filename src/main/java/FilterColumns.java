ee8be445e22b4c49f56ed9bd98ff4d1b083fe3e8
abdulkarim-hamidi <abdulkarim.hamidi@yahoo.com>
2023-02-22 22:49:14 +0000

    lab completed

--- src/main/java/FilterColumns.java
+++ src/main/java/FilterColumns.java
@@ -8,23 +8,29 @@ import java.util.List;
 /**
  * SQL sublanguage: DQL (Data Query Language)
  *
- * We have learned how to query all records from a table as well as filter the amount of records we get back utilizing the WHERE keyword.
+ * We have learned how to query all records from a table as well as filter 
+ * the amount of records we get back utilizing the WHERE keyword.
  *
- * In this lab we are going to learn how to filter the amount of columns that we want returned.
+ * In this lab we are going to learn how to filter the amount of columns 
+ * that we want returned.
  *
  * Let's take a look at the syntax to return an entire table again:
  * SELECT * FROM table_name;
  *
- * In the statement above, the * is the wildcard to retrieve all the columns in this specific table.
- * However, we can specify the columns that we want to display by the following syntax:
+ * In the statement above, the * is the wildcard to retrieve all the columns 
+ * in this specific table.
+ * However, we can specify the columns that we want to display by the 
+ * following syntax:
  * SELECT col_1, col_2, ...col_N FROM table_name;
  *
- * Additional reference material if needed: https://www.w3schools.com/sql/sql_select.asp
+ * Additional reference material if needed: 
+ * https://www.w3schools.com/sql/sql_select.asp
  */
 public class FilterColumns {
 
     /**
-     * problem 1: Write the SQL statement in the problem1.sql file to return only the 'firstname' column from the
+     * problem 1: Write the SQL statement in the problem1.sql file to return 
+     * only the 'firstname' column from the
      * site_user table
      *
      *      site_user table:
